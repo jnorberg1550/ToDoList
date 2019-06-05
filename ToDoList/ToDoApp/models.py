e8f702025bddc31acee32806d811e51d15b74a6e
JILL E NORBERG <35099642+jnorberg1550@users.noreply.github.com>
2019-06-05 00:49:10 -0700

    Add files via upload

--- ToDoList/ToDoApp/models.py
+++ ToDoList/ToDoApp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ListType(models.Model):
+    listtitle=models.Charfield (max_length=64)
+    listcreatedate=models.Datefield()
+    listcreatetime=models=models.TimeField()
+    listitem=models.Charfield (max_length=64)
+    listitemduedate=models.DateField()
+    listduetime=models=models.TimeField()
+    listpriority=models.Charfield (max_length=16)
+    
+li    class Meta:
+        db_table='todolist'
+        verbose_name_plural='ListTypes'
+
+    def __str__(self):
+        return self.listtitle
