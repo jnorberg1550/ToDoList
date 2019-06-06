fefd17249c73bb2c9958c55bbfa19583b902a4c0
JILL E NORBERG <35099642+jnorberg1550@users.noreply.github.com>
2019-06-05 23:50:25 -0700

    Add files via upload

--- ToDoList/ToDoApp/views.py
+++ ToDoList/ToDoApp/views.py
@@ -1,4 +1,9 @@
+from .models import ListType
 from django.shortcuts import render
 
 def index (request):
     return render(request, 'ToDoApp/index.html')
+
+def gettypes(request):
+    type_list=ListType.objects.all()
+    return render(request, 'ToDoApp/types.html' ,{'type_list' : type_list})
