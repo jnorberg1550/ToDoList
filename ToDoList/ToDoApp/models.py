d9d9fe745724877045d15d22c43d40d760a717f5
JILL E NORBERG <35099642+jnorberg1550@users.noreply.github.com>
2019-06-06 23:50:41 -0700

    Update models.py

--- ToDoList/ToDoApp/models.py
+++ ToDoList/ToDoApp/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class ListType(models.Model):
-    listtitle=models.Charfield (max_length=64)
-    listcreatedate=models.Datefield()
-    listcreatetime=models=models.TimeField()
-    listitem=models.Charfield (max_length=64)
+    listtitle=models.CharField (max_length=64)
+    listcreatedate=models.DateField()
+    listcreatetime=models.TimeField()
+    listitem=models.CharField (max_length=64)
     listitemduedate=models.DateField()
-    listduetime=models=models.TimeField()
-    listpriority=models.Charfield (max_length=16)
+    listduetime=models.TimeField()
+    listpriority=models.CharField (max_length=16)
     
 li    class Meta:
         db_table='todolist'
