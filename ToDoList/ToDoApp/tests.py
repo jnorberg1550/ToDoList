57e5bd29d75d83421475da942477b67846a38c1e
JILL E NORBERG <35099642+jnorberg1550@users.noreply.github.com>
2019-06-10 00:49:06 -0700

    Add files via upload

--- ToDoList/ToDoApp/tests.py
+++ ToDoList/ToDoApp/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
+from django.contrib auth.models.User
+from django.urls import reverse
+from models import ListType
+import datetime
 
-# Create your tests here.
+
+class ListTypeTest(TestCase):
+   #set up one time sample data
+  class ListTypeTest(TestCase):
+    def test_string(self):
+       type=ListType(listtitle='Shopping')
+       self.assertEqual(str(ListType), type.listtitle)
+
+    def test_string(self):
+       type=ListType(listcreatedate='6-10-2019')
+       self.assertEqual(str(ListType), type.listcreatedate)
+
+    def test_string(self):
+       type=ListType(listcreatetime='12:15')
+       self.assertEqual(str(ListType), type.listcreatetime)
+
+    def test_string(self):
+       type=ListType(listitem='toothpaste')
+       self.assertEqual(str(ListType), type.listitem)
+
+    def test_string(self):
+       type=ListType(listduedate='6-12-2019')
+       self.assertEqual(str(ListType), type.listduedate)
+
+    def test_string(self):
+       type=ListType(listduetime='15:00')
+       self.assertEqual(str(ListType), type.listduetime)
+
+    def test_string(self):
+       type=ListType(listpriority='Urgent')
+       self.assertEqual(str(ListType), type.listpriority)
+
+   
+   
+   
+   
+        
