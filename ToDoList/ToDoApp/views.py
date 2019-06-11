ab1d77584fa4c4081dca14fb04ebbcfcf9b93978
JILL E NORBERG <35099642+jnorberg1550@users.noreply.github.com>
2019-06-10 23:22:41 -0700

    Add files via upload

--- ToDoList/ToDoApp/views.py
+++ ToDoList/ToDoApp/views.py
@@ -1,13 +1,25 @@
 from .models import ListType
 from django.shortcuts import render
 
-    def index (request):
+def index (request):
     return render(request, 'ToDoApp/index.html')
 
-    def gettypes(request):
+def gettypes(request):
     type_list=ListType.objects.all()
     return render(request, 'ToDoApp/types.html' ,{'type_list' : type_list})
 
-    def getduedate(request):
+def getduedate(request):
     listitemduedate_list=ListType.objects.all()
     return render(request, 'ToDoApp/listitemduedate.html', {'listitemduedates_list': listitemduedates_list})
+
+def newList(request):
+     form=ListForm
+     if request.method=='POST':
+          form=ListForm(request.POST)
+          if form.is_valid():
+               post=form.save(commit=True)
+               post.save()
+               form=ListForm()
+     else:
+          form=ListForm()
+     return render(request, 'ToDoApp/newlistform,.html', {'form': form})
