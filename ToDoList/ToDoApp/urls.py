289a86a742dbdb30adaef9b6d3a11ef3877077b1
JILL E NORBERG <35099642+jnorberg1550@users.noreply.github.com>
2019-06-10 23:28:43 -0700

    Add files via upload

--- ToDoList/ToDoApp/urls.py
+++ ToDoList/ToDoApp/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns=[
     path('', views.index, name='index'),
     path('gettypes/', views.gettypes, name='types'),
+    path('newList/', views.newList name='newlist'),
 ]
