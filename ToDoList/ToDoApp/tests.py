fbbdf49e313ad664f18a0f9622e811a926fc0346
JILL E NORBERG <35099642+jnorberg1550@users.noreply.github.com>
2019-06-11 21:51:08 -0700

    Add files via upload

--- ToDoList/ToDoApp/tests.py
+++ ToDoList/ToDoApp/tests.py
@@ -3,38 +3,63 @@ from django.contrib auth.models.User
 from django.urls import reverse
 from models import ListType
 import datetime
+from .forms import NewListForm
 
 
 class ListTypeTest(TestCase):
    #set up one time sample data
   class ListTypeTest(TestCase):
     def test_string(self):
-       type=ListType(listtitle='Shopping')
+       type=ListType(listtitle=Shopping)
        self.assertEqual(str(ListType), type.listtitle)
 
     def test_string(self):
-       type=ListType(listcreatedate='6-10-2019')
+       type=ListType(listcreatedate= datetime.date (2019,6.10)
        self.assertEqual(str(ListType), type.listcreatedate)
 
     def test_string(self):
-       type=ListType(listcreatetime='12:15')
+       type=ListType(listcreatetime= datetime.time (12,15)
        self.assertEqual(str(ListType), type.listcreatetime)
 
     def test_string(self):
-       type=ListType(listitem='toothpaste')
+       type=ListType(listitem= 'toothpaste')
        self.assertEqual(str(ListType), type.listitem)
 
     def test_string(self):
-       type=ListType(listduedate='6-12-2019')
+       type=ListType(listduedate= datetime.date(2019,6,11)
        self.assertEqual(str(ListType), type.listduedate)
 
     def test_string(self):
-       type=ListType(listduetime='15:00')
+       type=ListType(listduetime= datetime.time (15,00)
        self.assertEqual(str(ListType), type.listduetime)
 
     def test_string(self):
-       type=ListType(listpriority='Urgent')
+       type=ListType(listpriority='urgent')
        self.assertEqual(str(ListType), type.listpriority)
+
+class NewListFormTest(TestCase):
+    def setUp(self):
+        self.user2=User.objects.create(username='user1', password='P@ssw0rd1')
+        self.type2=TechType.objects.create(listtitle= Saturday)
+    
+    def test_NewListForm (self):
+        data={
+            'listcreatedate' : datetime.date(2019,6,28),
+            'listcreatetime' : datetime.time(12,15),
+            'listpriority' : Urgent,
+         
+        }
+        form = NewListForm(data=data)
+        self.assertTrue(form.is_valid)
+
+    def test_NewListForm Invalid(self):
+        data={
+            'listcreatedate' : datetime.date(2019,6,28),
+            'listcreatetime' : datetime.time(12,15),
+            'listpriority' : Urgent,
+        }
+        form = NewListForm (data=data)
+        self.assertFalse(form.is_valid())    
 
    
    
